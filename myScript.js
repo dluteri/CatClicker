0cbf985586352bb2b7e392582dc338f32e170168
Donna Luteri <donna.luteri@gmail.com>
2018-09-13 11:08:10 -0500

    2nd revision

--- myScript.js
+++ myScript.js
@@ -1,9 +1,23 @@
 const catImg = document.getElementsByClassName('petCat')[0];
+const catImg2 = document.getElementsByClassName('petCat2')[0];
 const catCount = document.querySelector('.cat-counter');
+const catCount2 = document.querySelector('.cat-counter2');
 let catCountNum = Number(catCount.innerHTML);
+let catCountNum2 = Number(catCount2.innerHTML);
 
 catImg.addEventListener('click', function(){
+   // if(event.target.classList.contains('petCat') ) {
 console.log(catCountNum);
 catCountNum += 1;
 catCount.innerHTML=catCountNum;
-});  
+    });
+//}, false);  
+
+catImg2.addEventListener('click', function(){
+  //  if(event.target.classList.contains('petCat') ) {
+console.log(catCountNum2);
+catCountNum2 += 1;
+catCount2.innerHTML=catCountNum;
+});
+//}, false);  
+
